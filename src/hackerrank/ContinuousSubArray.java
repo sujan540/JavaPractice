50928d4b068c24a3665451f6335fac7124b8d671
Sujan Maharjan <thesujan540@gmail.com>
2021-04-30 13:30:46 -0500

    Update

--- src/hackerrank/ContinuousSubArray.java
+++ src/hackerrank/ContinuousSubArray.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ContinuousSubArray {
 
@@ -11,13 +12,14 @@ public class ContinuousSubArray {
 
         //List<Integer> input = Arrays.asList(1, 3, 2, 4, 6, 7, 5, 10);
         //List<Integer> input = Arrays.asList(1, 5, 10);
-        List<Integer> input = Arrays.asList();
+        //List<Integer> input = Arrays.asList(11, 3, 2, 4, 6, 7, 5, 10, 9);
+        List<Integer> input = Arrays.asList(11, 3, 2, 4, 6, 7, 5, 10);
+        // List<Integer> input = Arrays.asList();
 
         System.out.println(subArray(input));
     }
 
     public static List<Integer> subArray(List<Integer> array) {
-
         List<Integer> result = new ArrayList<>();
 
         int start = -1;
@@ -28,12 +30,14 @@ public class ContinuousSubArray {
                 continue;
             }
         }
-        for (int i = array.size() - 1; i > 1; i--) {
+        for (int i = array.size() - 1; i > 0; i--) {
             if (array.get(i) < array.get(i - 1) && (end == -1)) {
                 end = i;
                 continue;
             }
         }
+        System.out.println(start);
+        System.out.println(end);
         if (start == -1 && end == -1) {
             return new ArrayList<>();
         }
@@ -41,5 +45,10 @@ public class ContinuousSubArray {
             result.add(array.get(i));
         }
         return result;
+    }
+
+    public void subArrays(int[] array) {
+        IntStream.range(0, array.length)
+                .forEach(index -> System.out.println(index + " -> " + array[index]));
     }
 }
--- src/hackerrank/Palindrome.java
+++ src/hackerrank/Palindrome.java
@@ -1,5 +1,8 @@
 package hackerrank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Palindrome {
 
 	public static int reverse(int number) {
@@ -13,4 +16,68 @@ public class Palindrome {
 		return reverse;
 	}
 
+	public static void main(String[] args) {
+
+		String test = "racecar";
+		String test1 = "race car";
+		String test2 = "i      i";
+		String test3 = "a shoe a - not a palindrome";
+		String test4 = "a race car a";
+
+		System.out.println(isPalindrome(test));
+		System.out.println(isPalindrome(test1));
+		System.out.println(isPalindrome(test2));
+		System.out.println(isPalindrome(test3));
+		System.out.println(isPalindrome(test4));
+
+
+		System.out.println(longestPalindrome("wow i am in racecar house around noon"));
+
+	}
+
+	public static boolean isPalindrome(String text) {
+
+		int length = text.length();
+
+		String inputText = "";
+
+		for (int i = 0; i < length; i++) {
+			char c = text.charAt(i);
+			if (Character.isWhitespace(c)) {
+				continue;
+			}
+			inputText += c;
+		}
+		String compareText = "";
+		for (int j = length - 1; j >= 0; j--) {
+			char c = text.charAt(j);
+			if (Character.isWhitespace(c)) {
+				continue;
+			}
+			compareText += c;
+		}
+
+		return inputText.equals(compareText);
+	}
+
+
+	public static int longestPalindrome(String text) {
+
+		if (isPalindrome(text)) {
+			return text.length();
+		}
+
+		int maxLength = -1;
+		for (int i = 0; i < text.length(); i++) {
+			for (int j = i; j < text.length(); j++) {
+				String tempString = text.substring(i, j);
+				if (isPalindrome(tempString) && tempString.length() > maxLength) {
+					maxLength = tempString.length();
+				}
+			}
+		}
+
+		return maxLength;
+	}
 }
+
